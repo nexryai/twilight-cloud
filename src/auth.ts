c86738c1d9d31a0aea683440eea3cad19bb6c7ff
nexryai <61890205+nexryai@users.noreply.github.com>
2025-12-23 05:45:18 +0000

    Use basic authentications

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { nextCookies } from "better-auth/next-js";
 
 import { client, db } from "@/db";
 
@@ -7,4 +8,8 @@ export const auth = betterAuth({
     database: mongodbAdapter(db, {
         client,
     }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    plugins: [nextCookies()],
 });
