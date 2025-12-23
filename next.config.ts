7ce75f42928b102a4ea491a0dd76eb2c3d980cc3
nexryai <61890205+nexryai@users.noreply.github.com>
2025-12-23 07:41:42 +0000

    Fix headers

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,32 @@ const nextConfig: NextConfig = {
             bodySizeLimit: "10mb",
         },
     },
+    transpilePackages: ["ikaria.js"],
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "Cross-Origin-Opener-Policy",
+                        value: "same-origin",
+                    },
+                    {
+                        key: "Cross-Origin-Embedder-Policy",
+                        value: "require-corp",
+                    },
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*",
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET, POST, OPTIONS",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
